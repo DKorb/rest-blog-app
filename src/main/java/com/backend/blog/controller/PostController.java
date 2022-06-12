2954e8599eb4618fa80c6d09e81c797f2dd9b546
DKorb <dawid.korbecki@o2.pl>
2022-06-12 14:58:23 +0200

    Added PostController with createPost method

---
+++ src/main/java/com/backend/blog/controller/PostController.java
@@ -0,0 +1,26 @@
+package com.backend.blog.controller;
+
+import com.backend.blog.dto.PostDto;
+import com.backend.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+}
