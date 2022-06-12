e6a75b726c40d759b7ec37253889ac9b245b8b0a
DKorb <dawid.korbecki@o2.pl>
2022-06-12 15:06:59 +0200

    Refactored create post method in service implementation

--- src/main/java/com/backend/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/backend/blog/service/impl/PostServiceImpl.java
@@ -6,6 +6,8 @@ import com.backend.blog.repository.PostRepository;
 import com.backend.blog.service.PostService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -17,20 +19,27 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto createPost(PostDto postDto) {
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
+        return mapToDTO(newPost);
+    }
 
+    private PostDto mapToDTO(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto) {
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
 
-        Post newPost = postRepository.save(post);
-
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
-
-        return postResponse;
+        return post;
     }
 }
