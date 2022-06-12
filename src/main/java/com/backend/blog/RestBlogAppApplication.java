194bf12c378bcd6efaaa5fac0d175b52b6c639c0
DKorb <dawid.korbecki@o2.pl>
2022-06-12 14:03:17 +0200

    Added application.properties for database connection

--- src/main/java/com/backend/blog/RestBlogAppApplication.java
+++ src/main/java/com/backend/blog/RestBlogAppApplication.java
@@ -1,4 +1,4 @@
-package com.backend.restblogapp;
+package com.backend.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/backend/blog/RestBlogAppApplicationTests.java
+++ src/test/java/com/backend/blog/RestBlogAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.backend.restblogapp;
+package com.backend.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
