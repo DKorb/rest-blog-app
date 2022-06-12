a9883b5d6d49d6c0348acc48a6b9169297f38b4e
DKorb <dawid.korbecki@o2.pl>
2022-06-12 12:16:02 +0200

    Created a new project with dependencies

---
+++ src/main/java/com/backend/restblogapp/RestBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.backend.restblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBlogAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBlogAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/restblogapp/RestBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.restblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBlogAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
