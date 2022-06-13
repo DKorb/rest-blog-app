7be75748d8137ecbbd51687ade539012c5d91f22
DKorb <dawid.korbecki@o2.pl>
2022-06-13 22:27:08 +0200

    Added CommentController with implementation of create comment

---
+++ src/main/java/com/backend/blog/controller/CommentController.java
@@ -0,0 +1,26 @@
+package com.backend.blog.controller;
+
+import com.backend.blog.dto.CommentDto;
+import com.backend.blog.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/")
+public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long id,
+                                                    @RequestBody CommentDto commentDto) {
+        return new ResponseEntity<>(commentService.createComment(id, commentDto), HttpStatus.CREATED);
+    }
+}
