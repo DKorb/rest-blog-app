43b3cf76d7887db9491f115c9617197c23eccdac
DKorb <dawid.korbecki@o2.pl>
2022-06-13 21:52:38 +0200

    Added Comment entity with relationship to Post entity

---
+++ src/main/java/com/backend/blog/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.backend.blog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+}
--- src/main/java/com/backend/blog/entity/Post.java
+++ src/main/java/com/backend/blog/entity/Post.java
@@ -3,6 +3,8 @@ package com.backend.blog.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -23,5 +25,8 @@ public class Post {
 
     @Column(nullable = false)
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
 
 }
