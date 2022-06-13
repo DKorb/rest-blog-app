fc5f7673f1db3203669abdeb81c78b6d5216a86a
DKorb <dawid.korbecki@o2.pl>
2022-06-13 21:01:30 +0200

    Refactored const for hard-coded values for pagination and sorting

--- src/main/java/com/backend/blog/controller/PostController.java
+++ src/main/java/com/backend/blog/controller/PostController.java
@@ -3,11 +3,10 @@ package com.backend.blog.controller;
 import com.backend.blog.dto.PostDto;
 import com.backend.blog.dto.PostResponse;
 import com.backend.blog.service.PostService;
+import com.backend.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -26,10 +25,10 @@ public class PostController {
 
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
 
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
