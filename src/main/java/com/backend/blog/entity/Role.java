b2b09126a23a7283fc4f489d01a2c1062152c43e
DKorb <dawid.korbecki@o2.pl>
2022-06-16 11:54:08 +0200

    Added Role entity + repository with predefined methods

---
+++ src/main/java/com/backend/blog/entity/Role.java
@@ -0,0 +1,23 @@
+package com.backend.blog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 32)
+    private String name;
+
+}
---
+++ src/main/java/com/backend/blog/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.backend.blog.repository;
+
+import com.backend.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+}
